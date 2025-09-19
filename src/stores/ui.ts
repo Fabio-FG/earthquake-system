d4b4c0fd83e37cf9a7081e01780391d8e490e404
fabioFG <79693644+Fabio-FG@users.noreply.github.com>
2025-09-19 23:55:45 +0100

    Modified useMapStore and added types

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -1,6 +1,9 @@
 import { create } from "zustand";
 
-export const useMapStore = create((set) => ({
+export const useMapStore = create<{
+  showMap: boolean;
+  toggleMap: () => void;
+}>((set) => ({
   showMap: false,
   toggleMap: () => set((s) => ({ showMap: !s.showMap })),
 }));
