b802caaaae003a4de278f6eb3c8694facb5e7391
fabioFG <79693644+Fabio-FG@users.noreply.github.com>
2025-09-19 15:03:36 +0100

    Added tanstack query and its config

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import "./globals.css";
 
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -16,6 +17,8 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
