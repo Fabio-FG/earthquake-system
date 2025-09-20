482b387f20ed4b5df2fd752d2553401bc5bc3602
fabioFG <79693644+Fabio-FG@users.noreply.github.com>
2025-09-20 01:23:13 +0100

    imported leaflet

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
+
 
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
