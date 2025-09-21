71d3d0243f0082c5633c393c3d25a70e94ad2c97
fabioFG <79693644+Fabio-FG@users.noreply.github.com>
2025-09-21 16:48:00 +0100

    Changed the icon sizes

--- src/assets/icons/earthquake.icon.tsx
+++ src/assets/icons/earthquake.icon.tsx
@@ -7,41 +7,47 @@ export default function createEarthquakeIcon(
   magnitude: number | null,
   isSelected: boolean
 ) {
-  const getIconClasses = (mag: number) => {
+  const getIconClasses = (mag: number, selected: boolean) => {
+    if (selected) {
+      return {
+        containerClass: "w-5 h-5 bg-blue-500 border-transparent",
+        iconClass: "text-white text-2xl",
+      };
+    }
+
     if (mag >= 7)
       return {
-        containerClass: "w-10 h-10 bg-red-600 border-red-300",
+        containerClass: "w-8 h-8 bg-red-600 border-red-300",
         iconClass: "text-white text-2xl",
       };
     if (mag >= 6)
       return {
-        containerClass: "w-9 h-9 bg-orange-600 border-orange-300",
+        containerClass: "w-7 h-7 bg-orange-600 border-orange-300",
         iconClass: "text-white text-xl",
       };
     if (mag >= 5)
       return {
-        containerClass: "w-8 h-8 bg-yellow-600 border-yellow-300",
+        containerClass: "w-6 h-6 bg-yellow-600 border-yellow-300",
         iconClass: "text-white text-lg",
       };
     if (mag >= 4)
       return {
-        containerClass: "w-7 h-7 bg-yellow-500 border-yellow-200",
+        containerClass: "w-5 h-5 bg-yellow-500 border-yellow-200",
         iconClass: "text-white text-base",
       };
     return {
-      containerClass: "w-6 h-6 bg-green-600 border-green-300",
+      containerClass: "w-3 h-3 bg-green-600 border-green-300",
       iconClass: "text-white text-sm",
     };
   };
 
-  const { containerClass, iconClass } = getIconClasses(magnitude!);
+  const { containerClass, iconClass } = getIconClasses(magnitude!, isSelected);
 
   //leaflet doesnt accept react components so i need to convert this to HTML str
   const renderIcon = renderToString(
     <div
       className={cn(
         "border-2 rounded-full flex items-center justify-center shadow-lg",
-        isSelected && "border-white border-10",
         containerClass
       )}
     >
