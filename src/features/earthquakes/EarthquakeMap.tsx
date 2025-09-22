8fc77d37bbfeddc93d7108ae65903b227392aef3
fabioFG <79693644+Fabio-FG@users.noreply.github.com>
2025-09-22 02:41:35 +0100

    Added new base layers to the leaflet map options

--- src/features/earthquakes/EarthquakeMap.tsx
+++ src/features/earthquakes/EarthquakeMap.tsx
@@ -1,11 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { MdOutlineError } from "react-icons/md";
+import {
+  LayersControl,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  ZoomControl,
+} from "react-leaflet";
 import createEarthquakeIcon from "../../assets/icons/earthquake.icon";
-import { formatTime } from "../../helpers/formatTime";
-import { getEarthquakes } from "./api";
+import { formatTime } from "../../helpers/formatTime.helper";
+import { cn } from "../../utils/cn";
+import { useEarthquakeParams } from "./hooks/useEarthquakeParams";
+import useEarthquakes from "./hooks/useEarthquakes";
 import { useEarthquakeStore } from "./stores/earthquake.store";
-import type { EarthquakeParams } from "./types";
+import type { EarthquakeFeature } from "./types";
+const { BaseLayer } = LayersControl;
+
+import { ImSpinner3 } from "react-icons/im";
 
 function MapController() {
   const { selectedEarthquake } = useEarthquakeStore();
@@ -23,93 +36,143 @@ function MapController() {
 }
 
 export default function EarthquakeMap() {
-  const { selectedEarthquake } = useEarthquakeStore();
-  const queryParams: EarthquakeParams = { minmagnitude: 2 };
+  const { selectedEarthquake, setSelectedEarthquake } = useEarthquakeStore();
+  const { params } = useEarthquakeParams();
 
-  const {
-    data: earthquakes,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["earthquake"],
-    queryFn: () => getEarthquakes(queryParams),
-    staleTime: 1000 * 60 * 5,
+  const { earthquakes, isLoading, error, isFetching } = useEarthquakes({
+    ...params,
+    limit: undefined,
   });
 
-  const [center] = useState<[number, number]>([38.7169, -9.139]);
-
-  if (isLoading) {
-    return (
-      <div className="border-2 flex items-center justify-center w-screen h-screen">
-        <div className="text-lg">Loading Earthquake data...</div>
-      </div>
-    );
+  function handleQuake(quake: EarthquakeFeature) {
+    setSelectedEarthquake(quake);
   }
 
-  if (error) {
-    return (
-      <div className="border-2 flex items-center justify-center w-screen h-screen">
-        <div className="text-lg text-red-500">
-          Error while fetching earthquake data.
+  const [center] = useState<[number, number]>([29.636, 129.59]);
+
+  function renderSystemMessage() {
+    if (isLoading) {
+      return (
+        <div className=" flex h-auto z-2000 bg-main fixed ml-auto self-start px-2 py-4 border-black-500 items-center gap-2 text-white bottom-5 left-auto right-5">
+          <ImSpinner3 size={20} />
+          <p className="text-md">Loading data...</p>
         </div>
-      </div>
-    );
+      );
+    }
+
+    if (isFetching) {
+      return (
+        <div className=" flex h-auto z-2000 bg-main fixed ml-auto self-start px-2 py-4 border-black-500 items-center gap-2 text-white bottom-5 left-auto right-5">
+          <ImSpinner3 size={20} />
+          <p className="text-md">Updating data...</p>
+        </div>
+      );
+    }
+
+    if (error) {
+      return (
+        <div className=" flex h-auto z-2000 bg-main fixed ml-auto self-start px-2 py-4 border-black-500 items-center gap-2 text-white bottom-5 left-auto right-5">
+          <MdOutlineError />
+          <p className="text-md">Error while fetching data. Try later.</p>
+        </div>
+      );
+    }
   }
 
   return (
     <MapContainer
       center={center}
       zoom={3}
-      maxZoom={5}
-      scrollWheelZoom={true}
+      maxZoom={7}
       minZoom={2}
+      worldCopyJump={true}
+      bounceAtZoomLimits={true}
+      zoomControl={false}
+      zoomAnimation={true}
+      scrollWheelZoom={true}
       maxBounds={[
         [-90, -180],
         [90, 180],
       ]}
-      className="w-screen h-screen lg:w-full lg:h-screen md:w-full md:min-h-dvh"
+      className="w-screen h-[calc(100vh-80px)] lg:w-full lg:h-screen md:w-full md:min-h-dvh Z-10"
     >
-      <TileLayer
-        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <LayersControl position="topright" collapsed>
+        {/* OpenStreetMap */}
+        <BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+          />
+        </BaseLayer>
 
+        {/* Carto Light */}
+        <BaseLayer name="Carto Light">
+          <TileLayer
+            url="https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png"
+            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/'>CARTO</a>"
+          />
+        </BaseLayer>
+
+        <BaseLayer name="Ocean">
+          <TileLayer
+            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://leafletjs.com/examples/layers-control.html">USGS The National Map</a>'
+            url="https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
+          />
+        </BaseLayer>
+
+        {/* Google Maps */}
+        <BaseLayer name="Google Maps">
+          <TileLayer
+            url="http://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+            attribution="&copy; <a href='https://maps.google.com/'>Google</a>"
+          />
+        </BaseLayer>
+
+        {/* Google Satellite */}
+        <BaseLayer name="Satellite">
+          <TileLayer
+            url="http://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+            attribution="&copy; <a href='https://maps.google.com/'>Google</a>"
+          />
+        </BaseLayer>
+      </LayersControl>
+
+      {renderSystemMessage()}
+      <ZoomControl position="topright" />
       <MapController />
-
-      <Marker position={center}>
-        <Popup>
-          {earthquakes?.features[0]
-            ? `Most recent quake: ${earthquakes.features[0].properties.place}`
-            : "My location(Lisbon)"}
-        </Popup>
-      </Marker>
-
-      {/* <MarkerClusterGroup showCoverageOnHover={true}> */}
       {earthquakes &&
         earthquakes.features.map((quake) => {
           const [lon, lat] = quake.geometry.coordinates;
           return (
-            <Marker
-              key={quake.id}
-              position={[lat, lon]}
-              icon={createEarthquakeIcon(
-                quake.properties.mag,
-                !!selectedEarthquake
-              )}
-            >
-              <Popup className="m-0">
-                <div className="flex flex-col gap-0 m-0">
-                  <p className="mx-0">Location: {quake.properties.place}</p>
-                  <p className="mx-0">Magnitude: {quake.properties.mag}</p>
-                  <p className="mx-0">
-                    Time: {formatTime(quake.properties.time)}
-                  </p>
-                </div>
-              </Popup>
-            </Marker>
+            <div onClick={() => handleQuake(quake)} key={quake.id}>
+              <Marker
+                key={quake.id}
+                zIndexOffset={selectedEarthquake?.id == quake.id ? 100 : 0}
+                position={[lat, lon]}
+                icon={createEarthquakeIcon(
+                  quake.properties.mag,
+                  selectedEarthquake?.id == quake.id
+                )}
+                eventHandlers={{
+                  click: () => handleQuake(quake),
+                }}
+              >
+                <Popup className={cn("m-0 flex")}>
+                  <div className="flex flex-col gap-0 m-0">
+                    <p className="text-yellow">
+                      Location: {quake.properties.place}
+                    </p>
+                    <p className="mx-0">Magnitude: {quake.properties.mag}</p>
+                    <p className="mx-0">
+                      Time: {formatTime(quake.properties.time)}
+                    </p>
+                    <p>Depth: {`${quake.geometry.coordinates[2]} Km`}</p>
+                  </div>
+                </Popup>
+              </Marker>
+            </div>
           );
         })}
-      {/* </MarkerClusterGroup> */}
     </MapContainer>
   );
 }
